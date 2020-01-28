b94c745142e44f8a6023daafa19a4bf20792128e
tommygreim <tommygreim1@gmail.com>
2020-01-28 12:39:44 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/MemAlloc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MemAlloc.dir/main.cpp.obj"
+  "MemAlloc.exe"
+  "MemAlloc.exe.manifest"
+  "MemAlloc.pdb"
+  "libMemAlloc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MemAlloc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/MemAlloc.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MemAlloc.
+# This may be replaced when dependencies are built.
